91cf258568060cd0b226c9a5717011fc85aa3e22
Luis Pacheco <luis.ifgo@gmail.com>
2022-12-20 22:31:12 -0200

    Versao 1 Site apresentação

---
+++ Js/Js.js
@@ -0,0 +1,9 @@
+nomemv = prompt("Informe o nome da pessoa mais velha")
+idademv = prompt("Informe a idade da pessoa mais velha")
+nomemn = prompt("Informe o nome da pessoa mais nova")
+idademn = prompt("Informe a idade da pessoa mais nova")
+difIdade = idademv - idademn
+
+alert("A pessoa mais velha se chama: "+nomemv+"\nE tem "+idademv+
+    " anos.\n\nA pessoa mais nova se chama: "+nomemn+"\nE tem "+idademn+
+    " anos.\n\nA diferença de idade entre elas é de "+difIdade+" anos.")
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var customer = new Customer();
+customer.name = "Luis";
+Console.WriteLine(customer.name);
+
+ class Customer{
+    public int MyProperty { get; set; }
+}
